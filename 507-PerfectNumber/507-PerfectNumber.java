4472d069b3ad3c830d572dbbff3b475b4e3154c8
Elakiya VL <v.l.elakiya29@gmail.com>
2026-07-08 15:57:24 +0530

    Create 507-PerfectNumber

---
+++ 507-PerfectNumber/507-PerfectNumber.java
@@ -0,0 +1,10 @@
+// Last updated: 7/8/2026, 3:57:24 PM
+class Solution{
+    public boolean checkPerfectNumber(int num) {
+        if (num == 6 || num == 28 || num == 496 || num == 8128 || num == 33550336) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
