09fe641b38e67c63f390d11fbc79a0b999a2568d
Elakiya VL <v.l.elakiya29@gmail.com>
2026-07-08 15:56:52 +0530

    Create 883-CarFleet

---
+++ 883-CarFleet/883-CarFleet.java
@@ -0,0 +1,20 @@
+// Last updated: 7/8/2026, 3:56:51 PM
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = position.length;
+        int[][] cars = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            cars[i][0] = position[i];
+            cars[i][1] = speed[i];
+        }
+        Arrays.sort(cars, (a, b) -> b[0] - a[0]);
+        Stack<Double> stack = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            double time = (double)(target - cars[i][0]) / cars[i][1];
+            if (stack.isEmpty() || time > stack.peek()) {
+                stack.push(time);
+            }
+        }
+        return stack.size();
+    }
+}
