d00901b67181a7f8e4af322f63c63873fc17ffcc
Elakiya VL <v.l.elakiya29@gmail.com>
2026-07-08 15:57:30 +0530

    Create 440-KThSmallestInLexicographicalOrder

---
+++ 440-KThSmallestInLexicographicalOrder/440-KThSmallestInLexicographicalOrder.py
@@ -0,0 +1,25 @@
+# Last updated: 7/8/2026, 3:57:30 PM
+class Solution:
+    def findKthNumber(self, n: int, k: int) -> int:
+        def count(curr,n):
+            steps=0
+            first=curr
+            last=curr
+            while first<=n:
+                steps+=min(n+1,last+1)-first
+                first*=10
+                last=last*10+9
+            return steps
+        curr=1
+        k-=1
+        while k>0:
+            steps =count(curr,n)
+            if steps <=k:
+                curr+=1
+                k-=steps
+            else:
+                curr*=10
+                k-=1
+        return curr
+
+
