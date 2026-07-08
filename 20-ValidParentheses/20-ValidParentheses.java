9a2507889bbc379855ca0af986fe8b960fb7ec63
Elakiya VL <v.l.elakiya29@gmail.com>
2026-07-08 15:38:23 +0530

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.java
@@ -0,0 +1,23 @@
+// Last updated: 7/8/2026, 3:38:22 PM
+class Solution {
+    public boolean isValid(String s) {
+       Stack<Character>s1=new Stack<>();
+       for(char ch:s.toCharArray()){
+        if(ch=='(' || ch=='[' || ch=='{'){
+            s1.push(ch);
+        }
+        else{
+            if(s1.isEmpty()){
+                return false;
+            }
+            char top=s1.pop();
+
+            if((ch==')' && top!='(') || (ch=='}' && top!='{') || (ch==']' && top!='[')){
+                return false;
+            }
+        }
+        
+       }
+       return s1.isEmpty();
+    }
+}
